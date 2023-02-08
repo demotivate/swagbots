9dd59b1296e89ae17fe76759f7c67570d667d20d
demotivate <50751626+demotivate@users.noreply.github.com>
2023-02-07 17:06:31 -0700

    partial apriltags conversion

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swagbots_autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swagbots_autonomous.java
@@ -25,7 +25,31 @@ import java.util.ArrayList;
 @Autonomous(name = "Swagbots Autonomous", group = "swagbots")
 public class swagbots_autonomous extends LinearOpMode {
 
-//    April
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+    static final double FEET_PER_METER = 3.28084;
+
+    // Lens intrinsics
+    // UNITS ARE PIXELS
+    // NOTE: this calibration is for the C920 webcam at 800x448.
+    // You will need to do your own calibration for other configurations!
+    double fx = 578.272;
+    double fy = 578.272;
+    double cx = 402.145;
+    double cy = 221.506;
+
+    // UNITS ARE METERS
+    double tagsize = 0.166;
+
+//    int ID_TAG_OF_INTEREST = 18; // Tag ID 18 from the 36h11 family
+    int LEFT = 0;
+    int MIDDLE = 1;
+    int RIGHT = 2;
+
+    AprilTagDetection tagOfInterest = null;
+
+    //hardware
     private DcMotor arm;
     private Servo hand;
 //    private ColorSensor Coloursensor;
@@ -34,10 +58,7 @@ public class swagbots_autonomous extends LinearOpMode {
     private DcMotor TopLeft;
     private DcMotor BottomLeft;
 
-//    private OpenCvCamera camera;
-//    private AprilTagDetection aprilTagDetection;
-//    private DcMotor LiftMotor;
-
+    //vars
     private ElapsedTime runtime = new ElapsedTime();
 
     private int encoder;
@@ -65,6 +86,30 @@ public class swagbots_autonomous extends LinearOpMode {
      */
     @Override
     public void runOpMode() {
+
+        //april tag stuff
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+            }
+        });
+
+        telemetry.setMsTransmissionInterval(50);
+
         arm = hardwareMap.get(DcMotor.class, "Arm");
         hand = hardwareMap.get(Servo.class, "hand");
         TopRight = hardwareMap.get(DcMotor.class, "Top Right");
@@ -82,10 +127,121 @@ public class swagbots_autonomous extends LinearOpMode {
         hand.setPosition(0); /** check later if this start pos is correct **/
 
         // Put initialization blocks here.
-        waitForStart();
+//        waitForStart();
+        /*
+         * The INIT-loop:
+         * This REPLACES waitForStart!
+         */
+        while (!isStarted() && !isStopRequested())
+        {
+            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
 
-        while(opModeIsActive()){
-            RunSequence();
+            if(currentDetections.size() != 0)
+            {
+                boolean tagFound = false;
+
+                for(AprilTagDetection tag : currentDetections)
+                {
+                    if(tag.id == LEFT || tag.id == MIDDLE || tag.id == RIGHT)
+                    {
+                        tagOfInterest = tag;
+                        tagFound = true;
+                        break;
+                    }
+                }
+
+                if(tagFound)
+                {
+                    telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
+                    tagToTelemetry(tagOfInterest);
+                }
+                else
+                {
+                    telemetry.addLine("Don't see tag of interest :(");
+
+                    if(tagOfInterest == null)
+                    {
+                        telemetry.addLine("(The tag has never been seen)");
+                    }
+                    else
+                    {
+                        telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                        tagToTelemetry(tagOfInterest);
+                    }
+                }
+
+            }
+            else
+            {
+                telemetry.addLine("Don't see tag of interest :(");
+
+                if(tagOfInterest == null)
+                {
+                    telemetry.addLine("(The tag has never been seen)");
+                }
+                else
+                {
+                    telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                    tagToTelemetry(tagOfInterest);
+                }
+
+            }
+
+            telemetry.update();
+            sleep(20);
+        }
+
+        /*
+         * The START command just came in: now work off the latest snapshot acquired
+         * during the init loop.
+         */
+
+        /* Update the telemetry */
+        if(tagOfInterest != null)
+        {
+            telemetry.addLine("Tag snapshot:\n");
+            tagToTelemetry(tagOfInterest);
+            telemetry.update();
+        }
+        else
+        {
+            telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :(");
+            telemetry.update();
+        }
+
+        /* Actually do something useful */
+        if(tagOfInterest == null)
+        {
+            /*
+             * Insert your autonomous code here, presumably running some default configuration
+             * since the tag was never sighted during INIT
+             */
+            while(opModeIsActive()){
+                RunSequence();
+            }
+        }
+        else
+        {
+            /*
+             * Insert your autonomous code here, probably using the tag pose to decide your configuration.
+             */
+            while(opModeIsActive()){
+                RunSequence();
+            }
+
+            // e.g.
+            if(tagOfInterest.pose.x <= 20)
+            {
+                // do something
+            }
+            else if(tagOfInterest.pose.x >= 20 && tagOfInterest.pose.x <= 50)
+            {
+                // do something else
+            }
+            else if(tagOfInterest.pose.x >= 50)
+            {
+                // do something else
+            }
         }
 
         telemetry.update();
@@ -173,5 +329,16 @@ public class swagbots_autonomous extends LinearOpMode {
         BottomRight.setPower(1.1 * (pivot - y - x));
         TopLeft.setPower((-pivot - y - x));
         BottomLeft.setPower((pivot - y + x));
+    }
+
+    void tagToTelemetry(AprilTagDetection detection)
+    {
+        telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+        telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
+        telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
+        telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
+        telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
     }
 }
